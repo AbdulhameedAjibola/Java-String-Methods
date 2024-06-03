a9b1a05c2f77084ae7fee2c10c4904c46a0fbc84
AbdulhameedAjibola <105476350+AbdulhameedAjibola@users.noreply.github.com>
2024-06-03 21:12:18 +0400

    A Java file to reverse a string

--- reverse.java
+++ reverse.java
@@ -1,10 +1,24 @@
 import java.util.Arrays;
 import java.util.Scanner;
 public class reverse {
-    static void reverse(){
+    static void reverseMethod(String word){
         
+        char reverse[] = new char[word.length()];
+        int length = word.length() - 1;
+        int j = 0;
+        for(int i = length; i >= 0; i-- ){
+            
+            reverse[j] = word.charAt(i);
+            j++;
+        }
+        String newWord = new String(reverse);
+        System.out.println(newWord);
     }
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a word to be reversed");
+        String word = sc.nextLine();
 
+        reverseMethod(word);
     }
 }
